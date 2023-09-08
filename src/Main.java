a57855d03be62ed84d05388569bb5e2aeba522f3
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-08 21:26:13 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,33 @@ class demo{
     }
 }
 
+class aadhaar{
+    private int aadhaar_no;
+    private String name;
+    private String address;
+
+    public void setdata(int aadhaar_no, String name, String address){
+        this.aadhaar_no=aadhaar_no;
+        this.address=address;
+        this.name=name;
+    }
+     public void getdata(){
+        System.out.println("Welcome "+ name);
+        System.out.println("Name :"+ name);
+        System.out.println("Address :"+ address);
+        System.out.println("Aadhaar Number :"+aadhaar_no);
+     }
+}
+
 
 public class Main {
     public static void main(String[] args) {
         demo x;
+        aadhaar y;
+        y= new aadhaar();
         x= new demo();
         x.access();
+        y.setdata(47569562, "Anirudh Salaria", "Greater Kailash Jammu");
+        y.getdata();
     }
 }

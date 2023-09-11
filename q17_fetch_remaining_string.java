f2843b308f491b7b2849c61be8ccdaf5c5eda121
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-11 22:26:42 +0530

    updates

--- q17_fetch_remaining_string.java
+++ q17_fetch_remaining_string.java
@@ -1,5 +1,7 @@
 public class q17_fetch_remaining_string {
     public static void main(String[] args){
-        System.out.println(a.Subsequence(a.indexOf(args[0] + args[0].length + 1, a.length())));
+        String a = "How and what happened";
+        System.out.println(a.indexOf(args[0]));
+        // System.out.println(a.subSequence(a.indexOf(args[0] + args[0].length() + 1, a.length())));
     }
 }
--- q18_occurence_of_word.java
+++ q18_occurence_of_word.java
@@ -2,7 +2,7 @@ public class q18_occurence_of_word {
     public static void main(String[] args){
         String string = String.join(" ", args);
         String word = "is";
-        String temp[] = String.split("");
+        String temp[] = string.split("");
         int count=0;
         for(int i=0;i<temp.length;i++){
             if(word.equals(temp[i])){

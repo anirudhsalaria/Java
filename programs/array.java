c2652229627ed73dd7e6a57f4acce3003cc1efb7
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-20 23:59:01 +0530

    Update array.java

--- programs/array.java
+++ programs/array.java
@@ -1,9 +1,21 @@
+// public class array {
+//     public static void main(String[] args){
+//         String[] cities = {"jammu", "hyderabad", "bareilly"};
+
+//         for(int i=0; i<cities.length; i++){
+//             System.out.println(cities[i]);
+//         }
+//     }
+// }
 public class array {
     public static void main(String[] args){
-        String[] cities = {"jammu", "hyderabad", "bareilly"};
+        int[][] cities = {{1,2,3},{4,5,6}};
 
         for(int i=0; i<cities.length; i++){
-            System.out.println(cities[i]);
+            for(int j=0; j<cities[i].length; j++){
+                System.out.print(cities[i][j] + "\t");
+            }
+            System.out.println();
         }
     }
 }

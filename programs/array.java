bace95acd1f20ffff644da8fb8dfa3a5c6835480
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-20 23:55:49 +0530

    Create array.java

---
+++ programs/array.java
@@ -0,0 +1,9 @@
+public class array {
+    public static void main(String[] args){
+        String[] cities = {"jammu", "hyderabad", "bareilly"};
+
+        for(int i=0; i<cities.length; i++){
+            System.out.println(cities[i]);
+        }
+    }
+}

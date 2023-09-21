2f4f09cdc105031e7f12355b47c88acbea1b7c35
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-21 19:51:06 +0530

    update

--- programs/accmod.java
+++ programs/accmod.java
@@ -1,18 +1,31 @@
-class A{
-    private int a=15;
-    private void display(){
-        System.out.println("Anirudh");
-    }
-    public int access(){
-        return a;
-    }
+// class A{
+//     private int a=15;
+//     private void display(){
+//         System.out.println("Anirudh");
+//     }
+//     public int access(){
+//         return a;
+//     }
 
     
+// }
+
+// public class accmod {
+//     public static void main(String[] args) {
+//         A ex = new A();
+//         System.out.println(ex.access());
+//     }
+// }
+
+class A{
+    protected void display(){
+        System.out.println("Anirudh");
+    }
 }
 
-public class accmod {
+class accmod{
     public static void main(String[] args) {
         A ex = new A();
-        System.out.println(ex.access());
+        ex.display();
     }
 }
---
+++ programs/protect.java
@@ -0,0 +1,12 @@
+class A{
+    public void display(){
+        System.out.println("Anirudh");
+    }
+}
+
+class protect{
+    public static void main(String[] args) {
+        A ex = new A();
+        ex.display();
+    }
+}

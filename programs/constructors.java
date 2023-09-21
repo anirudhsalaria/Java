d7e3a2d61f9248ae615361874380c5efb8976b39
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-21 16:53:01 +0530

    Update constructors.java

--- programs/constructors.java
+++ programs/constructors.java
@@ -1,12 +1,28 @@
-public class constructors {
-    String name;
-    int age;
-    constructors(){     //default constructor
-        this.name="anirudh";
-        this.age=20;
-        System.out.println("Name: "+ name + "\t" +" Age: " +age);
+// public class constructors {
+//     String name;
+//     int age;
+//     constructors(){     //default constructor
+//         this.name="anirudh";
+//         this.age=20;
+//         System.out.println("Name: "+ name + "\t" +" Age: " +age);
+//     }
+//     public static void main(String[] args) {
+//         constructors ex = new constructors();
+//     }
+// }
+
+// overloading constructors 
+public class constructors{
+    constructors(String name){
+        System.out.println("Name: " + name);
+    }
+    constructors(String name, int age){
+        System.out.println("Name: "+ name);
+        System.out.println("Age: "+ age);
     }
     public static void main(String[] args) {
-        constructors ex = new constructors();
+        constructors ex1 = new constructors("Anirudh");
+        constructors ex2 = new constructors("Anirudh",20);
+
     }
 }

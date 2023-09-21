648a796f96543fe33ffc279da702a41e78f2fb64
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-21 14:58:33 +0530

    Create oops.java

---
+++ programs/oops.java
@@ -0,0 +1,12 @@
+public class oops {
+
+    String name = "Rajesh";
+    int age = 20;
+    
+    public static void main(String[] args) {
+        oops ex1 = new oops();
+        oops ex2 = new oops();
+        System.out.println("Name: " + ex1.name);
+        System.out.println("Age: " + ex2.age);
+    }
+}

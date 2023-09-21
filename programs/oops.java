5c2ff0b663d02f501ff7b171cda46be657a422c1
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-21 15:08:46 +0530

    Update oops.java

--- programs/oops.java
+++ programs/oops.java
@@ -1,12 +1,25 @@
-public class oops {
+// public class oops {
 
+//     String name = "Rajesh";
+//     int age = 20;
+    
+//     public static void main(String[] args) {
+//         oops ex1 = new oops();
+//         oops ex2 = new oops();
+//         System.out.println("Name: " + ex1.name);
+//         System.out.println("Age: " + ex2.age);
+//     }
+// }
+
+class Example3 {
     String name = "Rajesh";
     int age = 20;
-    
+}
+public class oops {
+
     public static void main(String[] args) {
-        oops ex1 = new oops();
-        oops ex2 = new oops();
-        System.out.println("Name: " + ex1.name);
-        System.out.println("Age: " + ex2.age);
+        Example3 ex3 = new Example3();
+        System.out.println("Name: " + ex3.name);
+        System.out.println("Age: " + ex3.age);
     }
 }

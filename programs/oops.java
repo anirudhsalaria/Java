fefce5991cee45d7f051d94dceb5c8fde67c4152
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-21 15:48:38 +0530

    Update oops.java

--- programs/oops.java
+++ programs/oops.java
@@ -11,15 +11,43 @@
 //     }
 // }
 
-class Example3 {
-    String name = "Rajesh";
-    int age = 20;
-}
-public class oops {
+// class Example3 {
+//     String name = "Rajesh";
+//     int age = 20;
+// }
+// public class oops {
 
+//     public static void main(String[] args) {
+//         Example3 ex3 = new Example3();
+//         System.out.println("Name: " + ex3.name);
+//         System.out.println("Age: " + ex3.age);
+//     }
+// }
+
+public class oops {
+    //public method
+     public void fruits() {
+        String fruits[] = {"Apple", "Banana", "Mango", "Strawberry"};
+        System.out.println("Fruits:");
+        for (int i=0; i<fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+    }
+    
+    //static method
+    static void vegetables() {
+        String vegies[] = {"Onion", "Potato", "Carrot", "Raddish"};
+        System.out.println("Vegetables:");
+        for (int i=0; i<vegies.length; i++) {
+            System.out.println(vegies[i]);
+        }
+    }
+    
     public static void main(String[] args) {
-        Example3 ex3 = new Example3();
-        System.out.println("Name: " + ex3.name);
-        System.out.println("Age: " + ex3.age);
+        oops ex5 = new oops();        //need to create object
+        ex5.fruits();
+        System.out.println();
+        
+        vegetables();                        // no need to create object
     }
 }

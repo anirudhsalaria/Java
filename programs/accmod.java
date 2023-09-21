12db59ca152a76d7895c80e48b8f37ba423ffd6a
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-21 19:22:56 +0530

    Create accmod.java

---
+++ programs/accmod.java
@@ -0,0 +1,17 @@
+class accmod{
+    private void display(){
+        System.out.println("Anirudh");
+    }
+    public static void main(String[] args) {
+        accmod ex = new accmod();
+        ex.display();
+    }
+
+}
+
+// public class accmod {
+//     public static void main(String[] args) {
+//         A ex = new A();
+//         ex.display();
+//     }
+// }

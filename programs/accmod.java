dc20482800e0f558a93c25b892156966135856a5
Anirudh Salaria <salariaanirudh@gmail.com>
2023-09-21 19:53:13 +0530

    Update accmod.java

--- programs/accmod.java
+++ programs/accmod.java
@@ -21,11 +21,16 @@ class A{
     protected void display(){
         System.out.println("Anirudh");
     }
+
+    private int b=11;
+    public int a=15;
 }
 
-class accmod{
+class accmod extends A{
     public static void main(String[] args) {
-        A ex = new A();
+        accmod ex = new accmod();
         ex.display();
+        System.out.println(ex.a);
+        // System.out.println(ex.b);
     }
 }
